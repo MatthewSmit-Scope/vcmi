d9f4926b8620f537cb01e94648c86ed1903249d9
Andrey Filipenkov <kambaladecapitator@gmail.com>
2026-05-11 14:16:26 +0300

    Update install_conan_dependencies.sh

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -5,7 +5,7 @@ FILENAME="$1.txz"
 DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
 
 downloadedFile="$RUNNER_TEMP/1.zip"
-curl -Lo "$downloadedFile" "https://github.com/vcmi/vcmi-dependencies/actions/runs/25542804987/artifacts/6874653030"
+curl -Lo "$downloadedFile" "https://github.com/vcmi/vcmi-dependencies/actions/runs/25542804987/artifacts/6874653030" -H "Authorization: Bearer $2"
 cd "$RUNNER_TEMP"
 unzip -q "$downloadedFile"
 conan cache restore *.txz
