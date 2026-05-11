94e6dfe17d33a4c57fecb66fa06910c158e2c5b9
Andrey Filipenkov <decapitator@ukr.net>
2026-05-11 15:18:23 +0300

    test

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -4,8 +4,12 @@ RELEASE_TAG="2026-03-11"
 FILENAME="$1.txz"
 DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
 
-downloadedFile="$RUNNER_TEMP/1.zip"
-curl -Lo "$downloadedFile" "https://github.com/vcmi/vcmi-dependencies/actions/runs/25542804987/artifacts/6874653030" -H "Authorization: Bearer $2"
+gh run download 25542804987 \
+            --repo vcmi/vcmi-dependencies \
+            --dir "$RUNNER_TEMP" \
+            --name "dependencies-android-arm64-v8a"
+#downloadedFile="$RUNNER_TEMP/1.zip"
+#curl -Lo "$downloadedFile" "https://github.com/vcmi/vcmi-dependencies/actions/runs/25542804987/artifacts/6874653030" -H "Authorization: Bearer $2"
 cd "$RUNNER_TEMP"
-unzip -q "$downloadedFile"
+unzip -q *.zip
 conan cache restore *.txz
