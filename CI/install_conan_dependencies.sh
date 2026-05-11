f4f48ce6276bf6440d7e043d10d2ee0e9a947528
Andrey Filipenkov <decapitator@ukr.net>
2026-05-11 12:12:55 +0300

    test

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -4,6 +4,8 @@ RELEASE_TAG="2026-03-11"
 FILENAME="$1.txz"
 DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
 
-downloadedFile="$RUNNER_TEMP/$FILENAME"
-curl -Lo "$downloadedFile" "$DOWNLOAD_URL"
-conan cache restore "$downloadedFile"
+downloadedFile="$RUNNER_TEMP/1.zip"
+curl -Lo "$downloadedFile" "https://github.com/vcmi/vcmi-dependencies/actions/runs/25542804987/artifacts/6874653030"
+cd "$RUNNER_TEMP"
+unzip -q "$downloadedFile"
+conan cache restore *.txz
